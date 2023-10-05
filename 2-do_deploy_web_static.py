3ca1e6c5615f78b7b5e8645424d7718fabe05c33
elfadili-ae <elfadili.ae@gmail.com>
2023-10-05 11:31:08 +0100

    Update: added do_pack

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Compress web_static files."""
 
+from fabric.api import local
 from fabric.api import run
 from fabric.api import put
 from fabric.api import env
+from datetime import datetime
 import os
 
 env.hosts = [
@@ -11,6 +13,25 @@ env.hosts = [
     '54.144.46.194'
 ]
 env.user = "ubuntu"
+
+
+def do_pack():
+    """Compress the web_static directory."""
+    if not os.path.exists("./versions/"):
+        local("mkdir ./versions/")
+
+    dt = datetime.now()
+    command = "sudo tar -cvzf"
+    result = local("{} ./versions/web_static_{}{}{}{}{}{}.tgz web_static"
+                   .format(command,
+                           dt.year,
+                           dt.month,
+                           dt.day,
+                           dt.hour,
+                           dt.minute,
+                           dt.second),
+                   capture=True)
+    print(result.stdout)
 
 
 def do_deploy(archive_path):
