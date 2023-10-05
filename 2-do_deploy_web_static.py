91f80a51ae8cc4703958115f594e3f675fde6c55
elfadili-ae <elfadili.ae@gmail.com>
2023-10-05 12:32:37 +0100

    Update: added try except

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,59 +15,26 @@ env.hosts = [
 env.user = "ubuntu"
 
 
-def do_pack():
-    """Compress the web_static directory."""
-    if not os.path.exists("./versions/"):
-        local("mkdir ./versions/")
-
-    dt = datetime.now()
-    command = "sudo tar -cvzf"
-    result = local("{} ./versions/web_static_{}{}{}{}{}{}.tgz web_static"
-                   .format(command,
-                           dt.year,
-                           dt.month,
-                           dt.day,
-                           dt.hour,
-                           dt.minute,
-                           dt.second),
-                   capture=True)
-    print(result.stdout)
-
-
 def do_deploy(archive_path):
     """Deply the pack to the server."""
     if not os.path.isfile(archive_path):
         return False
 
-    result = put(archive_path, "/tmp/")
-    if not result.succeeded:
-        return False
-    archive = archive_path.split("/")[-1]
-    arch_name = archive.split(".")[0]
-    release_path = "/data/web_static/releases/{}/".format(arch_name)
-    result = run("mkdir -p {}".format(release_path))
-    if not result.succeeded:
-        return False
-    result = run("tar -xzf /tmp/{} -C {}".format(archive, release_path))
-    if not result.succeeded:
-        return False
+    try:
+        put(archive_path, "/tmp/")
+        archive = archive_path.split("/")[-1]
+        arch_name = archive.split(".")[0]
+        release_path = "/data/web_static/releases/{}/".format(arch_name)
+        run("mkdir -p {}".format(release_path))
 
-    result = run("rm /tmp/{}".format(archive))
-    if not result.succeeded:
+        run("tar -xzf /tmp/{} -C {}".format(archive, release_path))
+        run("rm /tmp/{}".format(archive))
+        pth = "/data/web_static/releases"
+        run("mv {}/{}/web_static/* {}/{}".format(pth, arch_name, pth, arch_name))
+
+        run("rm -rf {}/{}/web_static".format(pth, arch_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(release_path))
+        return True
+    except Exception:
         return False
-    pth = "/data/web_static/releases"
-    result = run("mv {}/{}/web_static/* {}/{}".
-                 format(pth, arch_name, pth, arch_name))
-    if not result.succeeded:
-        return False
-    result = run("rm -rf {}/{}/web_static".format(pth, arch_name))
-    if not result.succeeded:
-        return False
-    result = run("rm -rf /data/web_static/current")
-    if not result.succeeded:
-        return False
-    result = run("ln -s {} /data/web_static/current".format(release_path))
-    if not result.succeeded:
-        return False
-    print("New version deployed!")
-    return True
