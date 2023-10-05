f480ca5e2abdf2b6f04310c8cc8f77833b5ae114
elfadili-ae <elfadili.ae@gmail.com>
2023-10-05 10:31:46 +0100

    Fix: moved unpacked files to parent

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,19 +4,18 @@
 from fabric.api import *
 from datetime import datetime
 import os
-import sys
 
 env.hosts = [
     '54.90.21.58',
     '54.144.46.194'
 ]
-env.user   = "ubuntu"
+env.user = "ubuntu"
+
 
 def do_deploy(archive_path):
     """Deply the pack to the server."""
     if not os.path.exists(archive_path):
         return False
-
 
     result = put(archive_path, "/tmp/")
     if not result.succeeded:
@@ -24,7 +23,8 @@ def do_deploy(archive_path):
     archive = archive_path.split("/")[-1]
     arch_name = archive.split(".")[0]
     release_path = "/data/web_static/releases/{}/".format(arch_name)
-    if run("mkdir {}".format(release_path)) is True:
+    result = run("mkdir -p {}".format(release_path))
+    if not result.succeeded:
         return False
     result = run("tar -xzf /tmp/{} -C {}".format(archive, release_path))
     if not result.succeeded:
@@ -33,11 +33,21 @@ def do_deploy(archive_path):
     result = run("rm /tmp/{}".format(archive))
     if not result.succeeded:
         return False
-    result = run("rm /data/web_static/current")
+    pth = "/data/web_static/releases"
+    result = run("mv -f {}/{}/web_static/* {}/{}".format(pth,
+                                                         arch_name,
+                                                         pth,
+                                                         arch_name))
+    if not result.succeeded:
+        return False
+    result = run("rm -rf {}/{}/web_static".format(pth, arch_name))
+    if not result.succeeded:
+        return False
+    result = run("rm -rf /data/web_static/current")
     if not result.succeeded:
         return False
     result = run("ln -s {} /data/web_static/current".format(release_path))
     if not result.succeeded:
         return False
-
+    print("New version deployed!")
     return True
